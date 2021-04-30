56ea6838c6e89f5a1196ed6889a0f436d2eb1f9c
Devin Ercolano <devin.ercolano@gmail.com>
2021-04-30 13:28:29 -0700

    Initial Commit

---
+++ MyGemTarot/MyGemTarotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGemTarotApp.swift
+//  MyGemTarot
+//
+//  Created by Devin Ercolano on 4/30/21.
+//
+
+import SwiftUI
+
+@main
+struct MyGemTarotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
